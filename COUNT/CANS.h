cbc749e460c301e88318f10b43339b2aabe1293c
lv-byte <61612393+lv-byte@users.noreply.github.com>
2020-04-05 13:00:50 +0800

    界面代码（只含类不包括库）

--- COUNT/CANS.h
+++ COUNT/CANS.h
@@ -1,19 +1,19 @@
 ﻿#pragma once
 
 
-// CADD 对话框
+// CANS 对话框
 
-class CADD : public CDialogEx
+class CANS : public CDialogEx
 {
-	DECLARE_DYNAMIC(CADD)
+	DECLARE_DYNAMIC(CANS)
 
 public:
-	CADD(CWnd* pParent = nullptr);   // 标准构造函数
-	virtual ~CADD();
+	CANS(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CANS();
 
 // 对话框数据
 #ifdef AFX_DESIGN_TIME
-	enum { IDD = IDD_ADD };
+	enum { IDD = IDD_ANS };
 #endif
 
 protected:
